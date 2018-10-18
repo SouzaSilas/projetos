d3ead657bc96c2381403812ac90c91da3c999005
Silas Souza <silassouza307@gmail.com>
2018-10-18 13:48:33 -0300

    Primeiro Commit da equipe(Silas, Rodrigo e Marcos). Responsáveis pelos módulos: Componente(aluno) e curso. Declaramos as propriedades das classes(Componente e Curso), já com o mapeamento de entidade, visando a questão do uso com o Hibernate posteriormente.

--- src/main/java/br/edu/cairu/app/web/integra/cairu/projetos/database/dbclass/Componente.java
+++ src/main/java/br/edu/cairu/app/web/integra/cairu/projetos/database/dbclass/Componente.java
@@ -4,11 +4,106 @@
  * and open the template in the editor.
  */
 package br.edu.cairu.app.web.integra.cairu.projetos.database.dbclass;
+        
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  *
  * @author everton
  */
-public class Componente {
+@Entity
+@Table(name = "aluno")
+public class Componente implements Serializable {
+    
+    @Id
+    @GeneratedValue
+    private Integer idAluno;
+    private String mat_aluno;
+    private String nome_aluno;
+    private String nome_social;
+    private String senha;
+
+    public Integer getIdAluno() {
+        return idAluno;
+    }
+
+    public void setIdAluno(Integer idAluno) {
+        this.idAluno = idAluno;
+    }
+
+    public String getMat_aluno() {
+        return mat_aluno;
+    }
+
+    public void setMat_aluno(String mat_aluno) {
+        this.mat_aluno = mat_aluno;
+    }
+
+    public String getNome_aluno() {
+        return nome_aluno;
+    }
+
+    public void setNome_aluno(String nome_aluno) {
+        this.nome_aluno = nome_aluno;
+    }
+
+    public String getNome_social() {
+        return nome_social;
+    }
+
+    public void setNome_social(String nome_social) {
+        this.nome_social = nome_social;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 41 * hash + Objects.hashCode(this.idAluno);
+        hash = 41 * hash + Objects.hashCode(this.mat_aluno);
+        hash = 41 * hash + Objects.hashCode(this.nome_aluno);
+        hash = 41 * hash + Objects.hashCode(this.nome_social);
+        hash = 41 * hash + Objects.hashCode(this.senha);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Componente other = (Componente) obj;
+        if (!Objects.equals(this.mat_aluno, other.mat_aluno)) {
+            return false;
+        }
+        if (!Objects.equals(this.nome_aluno, other.nome_aluno)) {
+            return false;
+        }
+        if (!Objects.equals(this.nome_social, other.nome_social)) {
+            return false;
+        }
+        if (!Objects.equals(this.senha, other.senha)) {
+            return false;
+        }
+        return Objects.equals(this.idAluno, other.idAluno);
+    }
     
 }
--- src/main/java/br/edu/cairu/app/web/integra/cairu/projetos/database/dbclass/Curso.java
+++ src/main/java/br/edu/cairu/app/web/integra/cairu/projetos/database/dbclass/Curso.java
@@ -5,10 +5,67 @@
  */
 package br.edu.cairu.app.web.integra.cairu.projetos.database.dbclass;
 
-/**
- *
- * @author everton
- */
-public class Curso {
-    
-}
+
+    import java.io.Serializable ;
+    import java.util.Objects ;
+    import javax.persistence.Entity ;
+    import javax.persistence.GeneratedValue ;
+    import javax.persistence.Id ;
+    import javax.persistence.Table ;
+
+    /**
+     *
+     * @author everton
+     */
+    @Entity
+    @Table(name = "curso")
+    public class Curso implements Serializable {
+
+        @Id
+        @GeneratedValue
+        private Integer idCurso;
+        private String nome;
+
+        public Integer getIdCurso() {
+            return idCurso;
+        }
+
+        public void setIdCurso(Integer idCurso) {
+            this.idCurso = idCurso;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 17 * hash + Objects.hashCode(this.idCurso);
+            hash = 17 * hash + Objects.hashCode(this.nome);
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final Curso other = (Curso) obj;
+            if (!Objects.equals(this.nome, other.nome)) {
+                return false;
+            }
+            return Objects.equals(this.idCurso, other.idCurso);
+        }
+
+    }
