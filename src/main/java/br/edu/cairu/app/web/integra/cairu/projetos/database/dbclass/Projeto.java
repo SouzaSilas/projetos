fcde6bdb6bd42f8518008250d60f53d56392c52f
Silas Souza <42219560+SouzaSilas@users.noreply.github.com>
2018-10-22 14:12:53 -0300

    Update Projeto.java

--- src/main/java/br/edu/cairu/app/web/integra/cairu/projetos/database/dbclass/Projeto.java
+++ src/main/java/br/edu/cairu/app/web/integra/cairu/projetos/database/dbclass/Projeto.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.edu.cairu.app.web.integra.cairu.projetos.database.dbclass;
 
 import java.io.Serializable;
@@ -11,10 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- *
- * @author everton
- */
 @Entity
 @Table(name = "projeto")
 public class Projeto implements Serializable {
